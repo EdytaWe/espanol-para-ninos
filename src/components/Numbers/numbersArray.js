3343504037effb6cc1998733dde65bcf0e607e1e
EdytaWE <edytawesolowska@gmail.com>
2022-04-15 17:54:54 +0200

    quiz classes

--- src/components/Numbers/numbersArray.js
+++ src/components/Numbers/numbersArray.js
@@ -12,24 +12,24 @@ const numbers = [
         number: "2",
         spain: "dos",
         polish: "dwa",
-        picture: "./img1/",
-        alternate: "./img2"
+        picture: "./img/numbers/2.png",
+        alternate: "./img/numbers/8.png"
     },
     {
         id: 3,
         number: "3",
         spain: "tres",
         polish: "trzy",
-        picture: "./img1/",
-        alternate: "./img2"
+        picture: "./img/numbers/3.png",
+        alternate: "./img/numbers/6.png"
     },
     {
         id: 4,
         number: "4",
         spain: "cuatro",
         polish: "cztery",
-        picture: "./img1/",
-        alternate: "./img2"
+        picture: "./img/numbers/4.png",
+        alternate:"./img/numbers/1.png"
     },
     {
         id: 5,
@@ -37,47 +37,53 @@ const numbers = [
         spain: "cinco",
         polish: "pięć",
         picture: "./img/numbers/5.png",
-        alternate: "./img/numbers/1.png"
+        alternate: "./img/numbers/2.png"
     },
     {
         id: 6,
         number: "6",
         spain: "seis",
         polish: "sześć",
-        picture: "./img1/",
-        alternate: "./img1"
+        picture: "./img/numbers/6.png",
+        alternate:"./img/numbers/10.png"
     },
     {
         id: 7,
         number: "7",
         spain: "siete",
         polish: "siedem",
-        picture: "./img/",
-        alternate: "./img"
+        picture: "./img/numbers/7.png",
+        alternate: "./img/numbers/9.png"
     },
     {
         id: 8,
         number: "8",
         spain: "ocho",
         polish: "osiem",
-        picture: "./img/",
-        alternate: "./img"
+        picture: "./img/numbers/8.png",
+        alternate:"./img/numbers/3.png"
     },
     {
         id: 9,
         number: "2",
         spain: "dos",
         polish: "dwa",
-        picture: "./img/",
-        alternate: "./img"
+        picture: "./img/numbers/9.png",
+        alternate:"./img/numbers/4.png"
     },
     {
         id: 10,
         number: "10",
         spain: "diez",
         polish: "dziesięć",
-        picture: "./img/",
-        alternate: "./img"
+        picture: "./img/numbers/10.png",
+        alternate:"./img/numbers/6.png"
     }
 ]
 export default numbers
+
+const numbersMemo = ["./img/numbers/1.png", "./img/numbers/1.png", "./img/numbers/2.png", "./img/numbers/2.png",
+    "./img/numbers/3.png", "./img/numbers/3.png", "./img/numbers/4.png", "./img/numbers/4.png", "./img/numbers/5.png",
+    "./img/numbers/5.png", "./img/numbers/6.png", "./img/numbers/6.png", "./img/numbers/7.png", "./img/numbers/7.png",
+    "./img/numbers/8.png", "./img/numbers/8.png", "./img/numbers/9.png", "./img/numbers/9.png", "./img/numbers/10.png",
+    "./img/numbers/10.png"]
--- src/components/base/Quiz.js
+++ src/components/base/Quiz.js
@@ -1,17 +1,19 @@
 import React, {useState} from 'react';
 import './quiz.scss';
 import {speak} from "./speak";
+import classnames from 'classnames';
 
 function Quiz({sound, img1, id1, img2, id2}) {
-    const [anime, setAnime] = useState(["quiz__img"])
+    const [shake, setShake] = useState(false)
     const check = (e) =>{
         if(e.id1 === 1 || e.id2 === 1){
            console.log(e.target)
             console.log("jest 1")
+            setShake(false);
 
         } else{
             console.log("jest 0")
-            setAnime(anime => [...anime, "shake__horizontal"])
+            setShake(true)
 
         }
     }
@@ -21,12 +23,12 @@ function Quiz({sound, img1, id1, img2, id2}) {
                 <span className="bi bi-megaphone-fill display-1" onClick={() => speak(`{${sound}`)}/>
                 <div className="quiz d-flex justify-content-center">
                     <div className="quiz__container mx-1 mx-sm-4 p-sm-3">
-                        <div className={anime} style={{backgroundImage: `url(${img1})`,
-                            backgroundRepeat: "no-repeat"}} onClick={(el) => check({id1})}></div>
+                        <div className={classnames('quiz__img', {'shake__horizontal': shake })} style={{backgroundImage: `url(${img1})`,
+                            backgroundRepeat: "no-repeat"}} onClick={(el) => check({id1})} />
                     </div>
                     <div className="quiz__container mx-1 mx-sm-4 p-sm-3">
-                        <div className="quiz__img shake__horizontal" style={{backgroundImage: `url(${img2})`,
-                            backgroundRepeat: "no-repeat"}} onClick={() => check({id2})}></div>
+                        <div className={classnames('quiz__img', {'shake__horizontal': shake })} style={{backgroundImage: `url(${img2})`,
+                            backgroundRepeat: "no-repeat"}} onClick={() => check({id2})} />
                     </div>
                 </div>
             </div>
